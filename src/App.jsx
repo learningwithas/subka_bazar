f90ee91444d39863bc2441bbd918a6dd07e0b5ca
Padmaksha Pranav <padmakshapranav@gmail.com>
2025-03-22 22:18:48 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ const App = () => {
       {/*  */}
       <Routes>
         <Route path="/" element={<Home />} key="home" />
+        <Route path="/subka_bazar" element={<Home />} key="home" />
         <Route path="/products" element={<Products />} key="products" />
         <Route
           path="/products/:id"
